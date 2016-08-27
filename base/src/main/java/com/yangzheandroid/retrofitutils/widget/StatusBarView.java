d4c877272bdac9ea29573c4f6eb80013cb4a5a39
Jalen <1439504683@qq.com>
2016-08-27 09:04:00 +0800

    状态栏适配工具类的添加

---
+++ base/src/main/java/com/yangzheandroid/retrofitutils/widget/StatusBarView.java
@@ -0,0 +1,37 @@
+package com.yangzheandroid.retrofitutils.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * =========================================================
+ *
+ *  版权: 个人开发Mr.Jalen  版权所有(c) ${YEAR}
+ *
+ *  作者:${USER}
+ *
+ *  版本: 1.0
+ *
+ *
+ *  创建日期 : ${DATE}  ${HOUR}:${MINUTE}
+ *
+ *
+ * 邮箱：Studylifetime@sina.com
+ *
+ * 描述:
+ *
+ *
+ * 修订历史:
+ *
+ * =========================================================
+ */
+public class StatusBarView extends View {
+    public StatusBarView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public StatusBarView(Context context) {
+        super(context);
+    }
+}
