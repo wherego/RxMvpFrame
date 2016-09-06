4b2824642a6397ff5d6712c37b4e0859a5312136
Jalen <1439504683@qq.com>
2016-09-06 08:11:21 +0800

    自定义View--初步开始

--- app/src/main/java/com/yangzheandroid/rxmvp/widget/custom/MyView.java
+++ app/src/main/java/com/yangzheandroid/rxmvp/widget/custom/MyView.java
@@ -17,11 +17,10 @@ import java.util.Random;
 import java.util.Set;
 
 /**
- * Author：Jalen on 2016/9/4 21:13
+ * Author：杨哲丶 on 2016/9/4 21:13
  * Contact: studylifetime@sina.com
  */
 public class MyView extends View {
-
 
     private String mTitleText;
     private int mTitleTextColor;
@@ -33,33 +32,33 @@ public class MyView extends View {
         this(context, null);
     }
 
-
     public MyView(Context context, AttributeSet attrs) {
-        this(context, attrs, 0);//需要定义为调用自己
-
+        this(context, attrs, 0);//注意一下，这里是this，不是默认的super
     }
-
 
     public MyView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-
-        initAttrs(attrs);
+        initAttrs(attrs);//初始化自定属性
 
         this.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
                 mTitleText = randomText();
+                //android中实现view的更新有两组方法，一组是invalidate，另一组是postInvalidate，
+                // 其中前者是在UI线程自身中使用，而后者在非UI线程中使用。
                 postInvalidate();
             }
-
-
         });
     }
 
+    /**
+     * 随机获得四个数字的方法
+     * @return
+     */
     private String randomText() {
         Set<Integer> set = new HashSet<Integer>();
         Random random = new Random();
-        while (set.size()<4){
+        while (set.size() < 4) {
             set.add(random.nextInt(10));
         }
 
@@ -72,26 +71,17 @@ public class MyView extends View {
     }
 
     private void initAttrs(AttributeSet attrs) {
-
         TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.MyView);
-
+        //默认的属性值都是用下划线分割的，官方规定。
         mTitleText = a.getString(R.styleable.MyView_titleText1);
-
         mTitleTextColor = a.getColor(R.styleable.MyView_titleTextColor1, Color.BLACK);
-
+        //我这里用的我自己的工具类，就是为了适配而已，给初学者普及一下...
         mTitleTextSize = a.getDimensionPixelSize(R.styleable.MyView_titleTextSize1, MyUtils.dp2px(getContext(), 16));
-
-        a.recycle();
-
-        /**
-         *  获取文本的宽和高
-         *
-         */
+        a.recycle();//记得释放
 
         mPaint = new Paint();
-
         mPaint.setTextSize(mTitleTextSize);
-
+        //获取文本的宽和高
         mBound = new Rect();
         mPaint.getTextBounds(mTitleText, 0, mTitleText.length(), mBound);
     }
@@ -99,11 +89,19 @@ public class MyView extends View {
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
         int width = getSize(widthMeasureSpec, true);
         int height = getSize(heightMeasureSpec, false);
         setMeasuredDimension(width, height);
     }
 
+    /**
+     * 获取高度和宽度
+     *
+     * @param measureSpec
+     * @param type  true 宽度，false 高度
+     * @return
+     */
     private int getSize(int measureSpec, boolean type) {
         int resultSize;
 
@@ -114,20 +112,19 @@ public class MyView extends View {
         } else {
             mPaint.setTextSize(mTitleTextSize);
             mPaint.getTextBounds(mTitleText, 0, mTitleText.length(), mBound);
-            resultSize = type ? getPaddingLeft() + getPaddingRight() + mBound.width() : getPaddingTop() + getPaddingBottom() + mBound.height();
+            resultSize = type ? getPaddingLeft() + getPaddingRight() + mBound.width()
+                    : getPaddingTop() + getPaddingBottom() + mBound.height();
         }
-
         return resultSize;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
-        mPaint.setColor(Color.YELLOW);
+        mPaint.setColor(Color.parseColor("#CCA4E3"));
         canvas.drawRect(0, 0, getMeasuredWidth(), getMeasuredHeight(), mPaint);
 
-        mPaint.setColor(mTitleTextColor);// TODO: 2016/9/4  getHeight() / 2 + mBound.height() 不理解
+        mPaint.setColor(mTitleTextColor);
         canvas.drawText(mTitleText, getWidth() / 2 - mBound.width() / 2, getHeight() / 2 + mBound.height() / 2, mPaint);
 
     }
